69493b12392a54b8b3605d928d18eea9ece6610c
sixdayC <2373046124@qq.com>
2025-12-16 13:11:15 +0800

    docs: update log for Lv3 and Lv4 completion

--- hello.s
+++ hello.s
@@ -1,9 +1,20 @@
   .text
   .globl main
 main:
-  li   t0, 6
-  seqz t0, t0
-  sub  t1, x0, t0
-  sub  t2, x0, t1
-  mv   a0, t2
+  addi sp, sp, -32
+entry:
+  li t0, 5
+  sw t0, 0(sp)
+  lw t0, 0(sp)
+  sw t0, 8(sp)
+  lw t0, 8(sp)
+  li t1, 10
+  add t0, t0, t1
+  sw t0, 12(sp)
+  lw t0, 12(sp)
+  sw t0, 4(sp)
+  lw t0, 4(sp)
+  sw t0, 16(sp)
+  lw a0, 16(sp)
+  addi sp, sp, 32
   ret
